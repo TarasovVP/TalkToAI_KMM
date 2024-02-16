b31ce24bc571bea86ee04f5f6c3534323c28f63a
vladimirtarasov <vptarasov1998@gmail.com>
2024-02-16 16:07:54 +0200

    Initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,58 @@
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.sqldelight)
+}
+
+kotlin {
+
+    val ktorVersion = "2.3.7"
+    val sqlDelightVersion = "1.5.5"
+    val coroutinesVersion = "1.7.3"
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+            implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
+        }
+    }
+}
+
+android {
+    namespace = "com.vnteam.talktoai.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+sqldelight {
+    database("AppDatabase") {
+        packageName = "com.vnteam.talktoai"
+    }
+}
